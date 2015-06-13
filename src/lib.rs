80b53339e44bed5b47cef3387c4ca89e2005e6c1
woxtu <woxtup@gmail.com>
2015-06-14 00:03:13 +0900

    Implement gimei

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ extern crate lazy_static;
 extern crate rand;
 extern crate yaml_rust;
 
+use rand::Rng;
+
 pub mod address;
 pub mod item;
 pub mod name;
@@ -13,3 +15,23 @@ pub mod name;
 pub use address::Address;
 pub use item::Item;
 pub use name::{Gender, Name};
+
+pub fn name() -> Name {
+  if rand::thread_rng().gen() {
+    Name::from_gender(Gender::Male)
+  } else {
+    Name::from_gender(Gender::Female)
+  }
+}
+
+pub fn male() -> Name {
+  Name::from_gender(Gender::Male)
+}
+
+pub fn female() -> Name {
+  Name::from_gender(Gender::Female)
+}
+
+pub fn address() -> Address {
+  Address::new()
+}
